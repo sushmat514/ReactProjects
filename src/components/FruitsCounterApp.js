958a24b1006155f87c3f79e7f2b2303db3aa6444
Sushma@711 <o170711@rguktong.ac.in>
2023-08-17 14:33:01 +0530

    FruitCounterApp js file is modified and setState problem is resolved

--- src/components/FruitsCounterApp.js
+++ src/components/FruitsCounterApp.js
@@ -4,24 +4,32 @@ export default class FruitsCounterApp extends Component {
     constructor(props){
         super(props)
         this.state={
-            mango:0,
+            mangoCount:0,
             banana:0
         }
+        this.onEatBanana = this.onEatBanana.bind(this)
+        // this.onEatMango = this.onEatMango.bind(this)
     }
     onEatMango(){
         console.log("mango consumed");
+        this.setState((prevState)=>({
+          mangoCount: prevState.mangoCount + 1
+        }),console.log(this.state.mangoCount))
     }
     onEatBanana(){
-        console.log("Banana eaten")
+        console.log("Banana eaten");
+        this.setState((prevState)=>({
+          banana: prevState.banana + 1
+        }))
     }
   render() {
     return (
       <div className='main-container'>
-        <h1>Bob ate 0 mangoes 0 bananas</h1>
+        <h1>Bob ate {this.state.mangoCount} mangoes {this.state.banana} bananas</h1>
         <div className="fruits-container">
             <div className="card-container">
                 <img src="https://assets.ccbp.in/frontend/react-js/mango-img.png" alt="mangos" className="img-class"/><br/>
-                <button className="button" onClick={this.onEatMango}>Eat Mango</button>
+                <button className="button" onClick={()=>(this.onEatMango())}>Eat Mango</button>
             </div>
             <div className='card-container'>
                 <img src="https://assets.ccbp.in/frontend/react-js/banana-img.png " alt="bananas" className="img-class"/><br/>
